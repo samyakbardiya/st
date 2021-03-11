be9a0598ce57aebf3d3b494b31665ac8f000ae93
Samyak Bardiya <samyak039@gmail.com>
2021-03-11 20:00:36 +0530

    2021-03-10

--- config.h
+++ config.h
@@ -119,29 +119,6 @@ static const char *colorname[] = {
   [13] = "#d3869b", /* magenta */
   [14] = "#8ec07c", /* cyan    */
   [15] = "#ebdbb2", /* white   */
-
-  // DRACULA
-  ///* 8 normal colors */
-  //[0] = "#000000", /* black   */
-  //[1] = "#ff5555", /* red     */
-  //[2] = "#50fa7b", /* green   */
-  //[3] = "#f1fa8c", /* yellow  */
-  //[4] = "#bd93f9", /* blue    */
-  //[5] = "#ff79c6", /* magenta */
-  //[6] = "#8be9fd", /* cyan    */
-  //[7] = "#bbbbbb", /* white   */
-  ///* 8 bright colors */
-  //[8]  = "#44475a", /* black   */
-  //[9]  = "#ff5555", /* red     */
-  //[10] = "#50fa7b", /* green   */
-  //[11] = "#f1fa8c", /* yellow  */
-  //[12] = "#bd93f9", /* blue    */
-  //[13] = "#ff79c6", /* magenta */
-  //[14] = "#8be9fd", /* cyan    */
-  //[15] = "#ffffff", /* white   */
-  ///* special colors */
-  //[256] = "#282a36", /* background */
-  //[257] = "#f8f8f2", /* foreground */
 };
 
 
@@ -271,19 +248,20 @@ static MouseShortcut mshortcuts[] = {
 
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
-#define TERMMOD (ControlMask|ShiftMask)
+#define TERMMOD (Mod1Mask|ShiftMask)
 
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
 	{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
+	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
+	/*{ XK_ANY_MOD,           XF86Calculator,      clippaste,      {.i =  0} },*/
+	{ TERMMOD,              XK_Y,           clipcopy,       {.i =  0} },
+	{ TERMMOD,              XK_P,           clippaste,      {.i =  0} },
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
-	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
 	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
 	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
-	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
-	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ TERMMOD,              XK_Return,      newterm,        {.i =  0} },
 };
